53339f416b3c73a4c318506e42c13e4f5276fa68
Andres Mendoza <andres.david.mm@hotmail.com>
2023-09-19 22:04:43 -0500

    adding update test

--- src/tests/city.test.js
+++ src/tests/city.test.js
@@ -1,6 +1,8 @@
 const request = require('supertest');
 const app = require('../app');
 
+let id;
+
 test("GET /cities debe retornar status 200", async () => {
   const res = await request(app).get('/cities');
   expect(res.status).toBe(200);
@@ -14,7 +16,21 @@ test("POST /cities debe crear una ciudad", async () => {
     isCapital: true,
   };
   const res = await request(app).post('/cities').send(city);
+  id = res.body.id;
   expect(res.status).toBe(201);
-  // expect(res.body.id).toBeDefined();
   expect(res.body).toHaveProperty('id');
+});
+
+test("PUT /cities/:id debe actualizar una ciudad", async () => {
+  const cityUpdated = {
+    name: "Bogota updated",
+  }
+  const res = await request(app).put(`/cities/${id}`).send(cityUpdated);
+  expect(res.status).toBe(200);
+  expect(res.body.name).toBe(cityUpdated.name);
+})
+
+test("DELETE /cities/:id debe eliminar una ciudad", async () => {
+  const res = await request(app).delete(`/cities/${id}`);
+  expect(res.status).toBe(204);
 });
--- src/tests/product.test.js
+++ src/tests/product.test.js
@@ -20,6 +20,25 @@ test("POST /products debe crear un producto", async () => {
   expect(res.body.id).toBeDefined();
 });
 
+test("PUT /products/:id debe actualizar un producto", async () => {
+  const productUpdated = {
+    name: "playstation updated"
+  }
+  const res = await request(app).put(`/products/${id}`).send(productUpdated);
+  expect(res.status).toBe(200);
+  expect(res.body.name).toBe(productUpdated.name);
+});
+
+test("GET /products/:id debe traer un producto", async () => {
+  const res = await request(app).get(`/products/${id}`);
+  expect(res.status).toBe(200);
+  expect(res.body.id).toBeDefined();
+});
+test("GET /products/:id con id que no existe debe retornar 404", async () => {
+  const res = await request(app).get(`/products/-1`);
+  expect(res.status).toBe(404);
+});
+
 test("DELETE /products/:id debe eliminar un producto", async () => {
   const res = await request(app).delete(`/products/${id}`);
   expect(res.status).toBe(204);
